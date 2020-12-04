636cfd783cbccc9ced5941e4aad66bca040fe0f8
kxxt <18085551+kxxt@users.noreply.github.com>
2020-12-04 22:56:42 +0800

    Update

--- app/forms.py
+++ app/forms.py
@@ -5,34 +5,34 @@ from app.models import User
 
 
 class LoginForm(FlaskForm):
-    username = StringField("User Name", validators=[DataRequired()])
-    password = StringField("Password", validators=[DataRequired()])
-    rememberme = BooleanField("Remember me")
-    submit = SubmitField('Sign in')
+    username = StringField("用户名", validators=[DataRequired()])
+    password = StringField("密码", validators=[DataRequired()])
+    rememberme = BooleanField("自动登录")
+    submit = SubmitField('登录')
 
 
 class RegistrationForm(FlaskForm):
     username = StringField('用户名', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = StringField("Password", validators=[DataRequired()])
-    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
+    email = StringField('邮箱', validators=[DataRequired(), Email()])
+    password = PasswordField("密码", validators=[DataRequired()])
+    password2 = PasswordField('重复密码', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('点我注册')
 
     def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user is not None:
-            return ValidationError("This username has already been taken.")
+        got_user = User.query.filter_by(username=username.data).first()
+        if got_user is not None:
+            raise ValidationError("此用户名已被使用!")
 
     def validate_email(self, email):
-        emial = User.query.filter_by(email=email.data).first()
-        if email is not None:
-            return ValidationError("The email address has already been taken.")
+        got_email = User.query.filter_by(email=email.data).first()
+        if got_email is not None:
+            raise ValidationError("此邮箱已被其他用户使用!")
 
 
 class EditProfileForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()])
-    about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
-    submit = SubmitField('Submit')
+    username = StringField("用户名", validators=[DataRequired()])
+    about_me = TextAreaField('个性签名', validators=[Length(min=0, max=140)])
+    submit = SubmitField('提交')
 
     def __init__(self, original_username, *args, **kwargs):
         super(EditProfileForm, self).__init__(*args, **kwargs)
@@ -42,4 +42,4 @@ class EditProfileForm(FlaskForm):
         if username.data != self.original_username:
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
-                raise ValidationError('Please use a different username.')
+                raise ValidationError('该用户名已被使用 , 请使用一个不同的用户名!')
